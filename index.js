931cae9d3b909f9a59022b52737515cdad34f2b2
ehasanrobin <ehasanrobin44@gmail.com>
2022-10-04 23:41:40 +0600

    server

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -5,7 +6,7 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 const nodemailerSendgrid = require("nodemailer-sendgrid");
 var jwt = require("jsonwebtoken");
-require("dotenv").config();
+
 app.use(cors());
 app.use(express.json());
 
